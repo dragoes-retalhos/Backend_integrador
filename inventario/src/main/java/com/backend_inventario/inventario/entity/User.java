0b08dc2667f9e57e19940b0c6e1307db055207d5
carlosbarros007 <carlosbarros723513@gmail.com>
2024-09-08 12:04:44 -0300

    alterando o tipo do id da entidade user para Long

--- inventario/src/main/java/com/backend_inventario/inventario/entity/User.java
+++ inventario/src/main/java/com/backend_inventario/inventario/entity/User.java
@@ -1,37 +1,41 @@
 package com.backend_inventario.inventario.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_user")
-    private int id;
+    private Long id;
 
     @Column(name = "name_user", length = 250)
     private String name;
 
     @Column(name = "email", length = 250, nullable = false, unique = true)
+    @Email(message = "E-mail ja esta cadastrado no sistema")
     private String email;
 
+    @NotNull(message = "Senha invalida")
     @Column(name = "password", length = 255, nullable = false)
     private String password;
 
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -59,11 +63,13 @@ public class User {
         this.password = password;
     }
 
+   
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + id;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
 
@@ -76,7 +82,10 @@ public class User {
         if (getClass() != obj.getClass())
             return false;
         User other = (User) obj;
-        if (id != other.id)
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
             return false;
         return true;
     }
