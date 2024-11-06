00693a521a5d830e7dfb88036dad759a4a0a0c57
carlosbarros007 <carlosbarros723513@gmail.com>
2024-11-06 20:25:51 -0300

    vai com deus

--- inventario/src/main/java/com/backend_inventario/inventario/entity/UserLoan.java
+++ inventario/src/main/java/com/backend_inventario/inventario/entity/UserLoan.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.backend_inventario.inventario.entity.Enum.StatusUserAndLoanEnum;
 import com.backend_inventario.inventario.entity.Enum.TypeUserLoanEnum;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
